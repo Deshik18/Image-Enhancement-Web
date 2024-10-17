153ec7a24fc21eb487d68846deef2841fa82190a
Deshik <deshiksingamsetty@gmail.com>
2024-10-17 06:16:26 +0530

    Final Change

--- algorithms/CLIP/CLIP/__init__.py
+++ algorithms/CLIP/CLIP/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 from .clip import *
-=======
-from .clip import *
->>>>>>> a7646af21c6cea9ad51970749a4f9f0ad59dbb68
--- test_algo.py
+++ test_algo.py
@@ -28,6 +28,6 @@ def algorithm_3(image_path):
     enhanced_image_path = dce_process_image(image_path)  # Use the directly imported function
     return enhanced_image_path
 
-# print(algorithm_1('tester.png'))
-# print(algorithm_2('tester.png'))
-# print(algorithm_3('tester.png'))
+print(algorithm_1('tester2.png'))
+print(algorithm_2('tester2.png'))
+print(algorithm_3('tester2.png'))
